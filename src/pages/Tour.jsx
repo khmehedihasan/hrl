ebaa5a2882b36772e71b988800d282eaa280b55f
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:42:42 +0600

    added

---
+++ src/pages/Tour.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Service from './Service';
+
+function Tour(){
+    return(
+        <Service>
+            <h1 className=' py-16 w-full text-center'>Tour</h1>
+
+        </Service>
+    )
+}
+
+export default Tour;
