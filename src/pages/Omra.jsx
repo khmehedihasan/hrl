3f428ef8e16c311af6ce513f4953f84030487e85
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:42:22 +0600

    added

---
+++ src/pages/Omra.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Service from './Service';
+
+function Omra(){
+    return(
+        <Service>
+            <h1 className=' py-16 w-full text-center'>Omra</h1>
+
+        </Service>
+    )
+}
+
+export default Omra;
