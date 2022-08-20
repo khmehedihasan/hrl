dd0016d6a44e287b7fd80ce139d4b47803da2116
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:40:35 +0600

    added

---
+++ src/pages/Visa.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Service from './Service';
+
+function Visa(){
+    return(
+        <Service>
+            <h1 className=' py-16 w-full text-center'>Visa</h1>
+
+        </Service>
+    )
+}
+
+export default Visa;
