fcd42ac57ae1f72f7c432451ef8695db17d239e2
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:41:29 +0600

    added

---
+++ src/pages/AirTicket.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Service from './Service';
+
+function AirTicket(){
+    return(
+        <Service>
+            <h1 className=' py-16 w-full text-center'>Air Ticket</h1>
+
+        </Service>
+    )
+}
+
+export default AirTicket;
