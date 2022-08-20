3e112aedf54c3b1c13974c11275dfa7473fdd11d
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:41:39 +0600

    added

---
+++ src/pages/Hajj.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Service from './Service';
+
+function Hajj(){
+    return(
+        <Service>
+            <h1 className=' py-16 w-full text-center'>Hajj</h1>
+
+        </Service>
+    )
+}
+
+export default Hajj;
