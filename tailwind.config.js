f0056daf9f9751852cd5be92dcf4237265177ae0
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:37:27 +0600

    added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bg-1":"url('../images/p1.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
