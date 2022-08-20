99d0afece97c7d059e98aa12655b1bb31cc53699
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-20 17:41:57 +0600

    added

---
+++ src/pages/Visit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Service from './Service';
+
+function Visit(){
+    return(
+        <Service>
+            <h1 className=' py-16 w-full text-center'>Visit</h1>
+
+        </Service>
+    )
+}
+
+export default Visit;
