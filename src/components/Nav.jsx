09927f7db208074a7d8d9b041f453219f1755840
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-25 18:05:23 +0600

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,8 +15,7 @@ function Nav(){
     }
     return(<>
         <div className=" hidden md:block">
-            <div className=" w-full h-10 bg-red-600 md:px-16 text-white text-sm flex items-center justify-between">
-                <div><i className="fa-solid fa-location-dot"></i><span> House 40, Road 06, Block C, Banasree, Rampura 1219 Dhaka, Bangladesh</span></div>
+            <div className=" w-full h-10 bg-red-600 md:px-16 text-white text-sm flex items-center justify-end">
                 <div><span>Help: </span> <i className="fa-solid fa-phone"></i><a href="tel:+88-02224405634"> 88-02224405634</a></div>
             </div>
             <div className="  sticky top-0 left-0">
@@ -35,10 +34,10 @@ function Nav(){
                             <div onMouseLeave={()=>setDrop('hidden')} className={`${drop} w-52 absolute top-14 py-2 px-1 rounded-md bg-red-600 flex gap-2 flex-col text-white`}>
                                 <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/visa">Visa</NavLink>
                                 {/* <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/visit">Visit</NavLink> */}
-                                <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/airTicket">Air Ticket</NavLink>
                                 <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/hajj">Hajj</NavLink>
                                 <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/omra">Omra</NavLink>
                                 <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/tour">Tour</NavLink>
+                                <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/airTicket">Air Ticket</NavLink>
 
                             </div>
                         </NavLink>
@@ -67,10 +66,10 @@ function Nav(){
                         <div className=" w-full h-4 -ml-5 border-t box-content px-8 border-white bg-none"></div>
                             <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/visa">Visa</NavLink>
                             {/* <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/visit">Visit</NavLink> */}
-                            <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/airTicket">Air Ticket</NavLink>
                             <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/hajj">Hajj</NavLink>
                             <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/omra">Omra</NavLink>
                             <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/tour">Tour</NavLink>
+                            <NavLink className={({isActive})=> isActive?"bg-red-700 pl-2 rounded-md":" hover:bg-red-700 pl-2 rounded-md"} to="/service/airTicket">Air Ticket</NavLink>
 
                         </div>
                     </NavLink>
