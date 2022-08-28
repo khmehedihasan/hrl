7f0c66e76b71ec6a6e1d9f2a4b8f0444c387829f
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-28 16:31:15 +0600

    update

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,16 @@
 import { combineReducers } from 'redux';
 import info from './addInfo';
 import slider from './addSlider';
+import category from './category';
+import packag from './package';
 
 
 const rootReducer = combineReducers({
     info,
-    slider
+    slider,
+    category,
+    packag
+
 
 });
 
