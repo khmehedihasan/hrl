3aadf80b7a55fe8301df74e91beb0b069e1a0281
khmehedihasan <mehedihasan47341@gmail.com>
2022-08-28 16:31:23 +0600

    update

--- src/store/actions/addInfo.js
+++ src/store/actions/addInfo.js
@@ -5,3 +5,11 @@ export function ADD_INFO(data){
 export function ADD_SLIDER(data){
     return({type:'ADD_SLIDER',data});
 }
+
+export function ADD_CATEGORY(data){
+    return({type:'ADD_CATEGORY',data});
+}
+
+export function ADD_PACKAGE(data){
+    return({type:'ADD_PACKAGE',data});
+}
