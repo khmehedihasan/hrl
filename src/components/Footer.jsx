ef5cabf66907e3468908e3f894769cd274c6a51a
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-01 11:51:44 +0600

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import logo from '../assets/images/logo.png';
 
 
 
@@ -21,8 +22,18 @@ function Footer(){
         <div className=' w-full bg-slate-600 '>
             <div className=' w-full p-2 mx-auto md:w-[700px] lg:w-[1000px] flex flex-col md:flex-row justify-between'>
                     <div className=' w-full h-auto md:h-60 pt-8 flex items-center flex-col md:flex-row justify-between gap-10 md:gap-0'>
-                    <div className=' w-72 h-auto  text-center text-white flex flex-col gap-4'>
-                        <h1 className=' text-2xl uppercase'> Holy Review</h1>
+                    <div className=' w-72 h-auto  text-center text-white flex flex-col justify-center items-center gap-4'>
+                        <img className=' w-[200px]' src={logo} alt=" " />
+                        {/* <h1 className=' text-2xl uppercase'> Holy Review</h1> */}
+                    </div>
+                    <div className=' w-72 h-auto  text-center text-white flex flex-col justify-center items-center gap-4'>
+                        {/* <h1 className=' text-2xl'>OTHER PAGES</h1>
+                        <NavLink to="/about" ><h1>About Us</h1></NavLink>
+                        
+                        <NavLink to=" " ><h1>Our Services</h1></NavLink>
+                        
+                        <NavLink to="/contact" ><h1>Contact Us</h1></NavLink> */}
+
                         <h3>Connect With Our Social Media</h3>
                         <div className=' flex gap-4 justify-center text-3xl'>
                             <a href="https://www.facebook.com/Holyreview/?ref=page_internal" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-square-facebook"></i></a>
@@ -30,14 +41,6 @@ function Footer(){
                             <a href=" " target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-square-instagram"></i></a>
                             <a href=" " target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
                         </div>
-                    </div>
-                    <div className=' w-72 h-auto  text-center text-white flex flex-col gap-4'>
-                        <h1 className=' text-2xl'>OTHER PAGES</h1>
-                        <NavLink to="/about" ><h1>About Us</h1></NavLink>
-                        
-                        <NavLink to=" " ><h1>Our Services</h1></NavLink>
-                        
-                        <NavLink to="/contact" ><h1>Contact Us</h1></NavLink>
                         
                     </div>
                     <div className=' w-80 h-auto text-white text-center md:text-left flex flex-col gap-2 md:-mr-16'>
